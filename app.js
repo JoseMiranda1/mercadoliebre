1110d80d925be9260694a1ab018200f5c989b2d2
JoseMiranda1 <josemiranda.rp@hotmail.com>
2021-12-17 15:29:50 -0300

    agregue process PORT

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express= require('express');
 const app = express();
 const path = require('path');
 app.use(express.static(path.resolve(__dirname,"public")));
-app.listen(3000, ()=> console.log('iniciando express'));
+app.listen(process.env.PORT || 3000, ()=> console.log('iniciando express'));
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, "views/home.html")));
 app.get('/login',(req,res)=> res.sendFile(path.join(__dirname,"views/login.html")));
 app.get("/register",(req, res)=> res.sendFile(path.join(__dirname,"views/register.html")))
